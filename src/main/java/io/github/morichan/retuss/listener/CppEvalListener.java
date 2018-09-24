a675b3fc5745365f754ff40789e8402515fbba80
kuriharashun <hm15019@student.miyazaki-u.ac.jp>
2018-09-24 14:50:57 +0900

    試験運用

--- src/main/java/io/github/morichan/retuss/listener/CppEvalListener.java
+++ src/main/java/io/github/morichan/retuss/listener/CppEvalListener.java
@@ -14,15 +14,33 @@ public class CppEvalListener extends CPP14BaseListener{
     private Cpp cpp = new Cpp();
     private String className; //
 
-    //classNameを抽出してくれるメソッド
-    @Override
-    public void exitUnqualifiedid(CPP14Parser.UnqualifiedidContext ctx) {
-        for(int i=0;i<ctx.getChildCount();i++) {
-            if(ctx.getChild(i) instanceof TerminalNodeImpl) {
-                className=ctx.getChild(0).getText();
-            }
-        }
-    }
+
+
+    //最初に探索をはじめた時の初期の構文で必要なものを準備
+
+    @Override public void enterTranslationunit(CPP14Parser.TranslationunitContext ctx) { }
+
+    @Override public void enterClasshead(CPP14Parser.ClassheadContext ctx) { }
+
+    @Override public void enterClassname(CPP14Parser.ClassnameContext ctx) { }
+
+
+
+
+
+    //抽出してくれるメソッドの処理の例
+//    @Override
+//    public void exitUnqualifiedid(CPP14Parser.UnqualifiedidContext ctx) {
+//        for(int i=0;i<ctx.getChildCount();i++) {
+//            if(ctx.getChild(i) instanceof TerminalNodeImpl) {
+//                className=ctx.getChild(0).getText();
+//            }
+//        }
+//    }
+
+
+
+
 
     public Cpp getCpp() {
         return cpp;
